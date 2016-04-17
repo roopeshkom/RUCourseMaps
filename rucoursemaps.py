d8af4323ea2fb4ec363f38c2b7888919a923474d
Rooopeswar Kommalapati <roopeshkom@gmail.com>
2016-04-17 01:45:15 -0400

    Got the method to return 2 strings, one of which is the classes, and the other consists of an alternating series of prereqs and its corresponding class. It currently works for any depeartment as long as the appropriate json is placed in a file given to the program as a argv[1]

--- rucoursemaps.py
+++ rucoursemaps.py
@@ -2,20 +2,35 @@ from sys import argv
 import json
 import re
 
-with open(argv[1]) as inp:
-	parsed_json = json.loads(inp.read())
-	prereqs = []; courses = []
-	for i in parsed_json:
-		courses.append(i.get('courseNumber'))
-		text = i.get('preReqNotes')
-		if not (text is None):
-			for m in re.finditer("198:... ", str(text)):
-				if not (m is None):
-					prereqs.append((m.group(), "%s:%s" % (i.get('subject'), i.get('courseNumber'))))
-				else:
-					prereqs.append(('nothing', "%s:%s" % (i.get('subject'), i.get('courseNumber'))))
-		else:
-			prereqs.append(('nothing', "%s:%s" % (i.get('subject'), i.get('courseNumber'))))
+def getLists(depnum):
+	if depnum == -1:
+		return None
 
-	print "Courses:\n%s" % courses
-	print "Prereqs:\n%s" % prereqs
+	with open(argv[1]) as inp:
+		parsed_json = json.loads(inp.read())
+		prereqs = []; courses = []
+		for i in parsed_json:
+			text = i.get('preReqNotes')
+			if not (text is None):
+				for m in re.findall("198:...", str(text)):
+					if not (m is None):
+						prereqs.append(m[4:7])
+					else:
+						prereqs.append('NULL')
+					prereqs.append(i.get('courseNumber'))
+			else:
+				prereqs.append('NULL')
+				prereqs.append(i.get('courseNumber'))
+
+			courses.append(i.get('courseNumber'))
+
+	for i in courses:
+		i.strip('u')
+	for i in prereqs:
+		i.strip('u')
+
+	ret = []
+	ret.append(', '.join(courses))
+	ret.append(', '.join(prereqs))
+	return ret
+
