270cdd58fd812e5b4b037bba814f34ff97a30f92
Rooopeswar Kommalapati <roopeshkom@gmail.com>
2016-04-17 04:41:44 -0400

    Added beautiful soup

--- rucoursemaps.py
+++ rucoursemaps.py
@@ -1,18 +1,22 @@
 import json
 import re
 
-
 def getLists(depnum):
 	if depnum == -1:
 		return None
+		
+	href = "http://sis.rutgers.edu/soc/courses.json?campus=NB&semester=92016&level=U&subject="
+	href += str(depnum)
+	print href
 
 	with open("testdata.txt") as inp:
-		parsed_json = json.loads(inp.read())
+		text = inp.read()
+		parsed_json = json.loads(text)
 		prereqs = []; courses = []
 		for i in parsed_json:
-			text = i.get('preReqNotes')
-			if not (text is None):
-				for m in re.findall("198:...", str(text)):
+			reqs = i.get('preReqNotes')
+			if not (reqs is None):
+				for m in re.findall("198:...", str(reqs)):
 					if not (m is None):
 						prereqs.append(m[4:7])
 					else:
@@ -33,3 +37,6 @@ def getLists(depnum):
 	ret.append(', '.join(courses))
 	ret.append(', '.join(prereqs))
 	return ret
+
+a, b = getLists(122)
+print a; print b
