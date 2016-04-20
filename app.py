71c2e235136c5ae0a7d9dc6cf4b8ebbc42e8e794
BTRUR <tarparab@outlook.com>
2016-04-20 19:18:17 -0400

    Added movement of course names. It now sends from app.py to graph.index.

--- app.py
+++ app.py
@@ -7,15 +7,21 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template("index.html") #efdtur form
-    if request.method == 'POST':
-        deptnum = request.form.get('deptnum')
-        courses,prereqs = getLists(deptnum) #getLiss(deptnum)
-        # print courses
-        # print prereqsfdsafd
-        return render_template("graphs.html",courses=courses,prereqs=prereqs)
-
+        return render_template("index.html")
+    if request.method == 'POST':                    #when post happens
+        deptnum = request.form.get('deptnum')       #get deptnum
+        courses,prereqs,names = getLists(deptnum)   #returns the array of 3 things
+        print "\\\\\\\\\\\\\\\\\\LIST OF courses\\\\\\\\\\\\\\"
+        print courses
+        print "\\\\\\\\\\\\\\\\\\LIST OF prereqs\\\\\\\\\\\\\\"
+        print prereqs
+        print "\\\\\\\\\\\\\\\\\\LIST OF names\\\\\\\\\\\\\\\\"
+        print names    
+        return render_template("graphs.html",courses=courses,prereqs=prereqs,names=names)
+        #print stuff to debug
 
 if __name__ == '__main__':
     app.run(debug=True)
-#special thanks to Reavor and
+
+#Special thanks to Reavor, Sakaib, AP, and Kartik
+# -Bhargav Tarpara
